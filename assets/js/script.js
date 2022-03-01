f7da13764b7611876d48b050888eafc383fe3e2b
mehedifoysal <mehedifoysal8@gmail.com>
2022-03-01 10:27:16 +0600

    phone details modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ const displayPhones = phones => {
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">Brand: ${phone.brand}</p>
-                    <button data-bs-toggle="modal" data-bs-target="#phone-details" onclick="phoneDetails(${phone.slug})" class="btn-default">View More</button>
+                    <button data-bs-toggle="modal" data-bs-target="#phone-details" onclick="phoneDetails('${phone.slug}')" class="btn-default">View More</button>
                 </div>
             </div>`;
         phoneGrid.appendChild(phoneCard);
@@ -42,13 +42,41 @@ const phoneDetails = async phoneSlug => {
     try {
         const res = await fetch(`https://openapi.programming-hero.com/api/phone/${phoneSlug}`);
         const data = await res.json();
+        console.log(data);
         displayPhoneDetails(data.data);
     } catch (error) {
         console.log(error);
     }
 }
 
+const displayPhoneDetails = phoneDetails =>{
+    const phoneDetailsModal = document.querySelector("#phone-details");
+    phoneDetailsModal.textContent = "";
+    const {chips, description, features, images, phone_name, price, screen, storage, slug} = phoneDetails;
+    phoneDetailsModal.innerHTML = `
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">${phoneDetails.phone_name}</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="${phoneDetails.image}" alt="${phoneDetails.phone_name}" class="img-fluid">
+                        </div>
+                        <div class="col-md-8">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
 
+}
 
 
 
